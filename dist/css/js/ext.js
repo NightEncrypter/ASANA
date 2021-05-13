eeaaf2757f2c9eac358f534a3135e0251ebab918
MANAS <manasrathore2342@gmail.com>
2021-05-13 14:04:16 +0530

    First Commit

---
+++ dist/css/js/ext.js
@@ -0,0 +1,47 @@
+new Glider(document.querySelector('.glider-1'), {
+    // how many item you want to show
+    slidesToShow: 1,
+    // touch supporting option
+    draggable: true,
+    // Specify the selector
+    dots: '#dots-1',
+
+    // Left right arrows selectors
+    arrows: {
+        prev: '.glider-prev-1',
+        next: '.glider-next-1',
+    },
+});
+
+
+
+// const check = document.querySelector('#tick');
+// const buttn = document.querySelector('#try-btn');
+
+//     buttn.addEventListener('mousemove', enterBtn);
+
+    
+//     function enterBtn() {
+
+//         setTimeout(function (e) {
+//             let x = Math.random() * 200;
+//             let y = Math.random() * 150;
+//             let z = Math.random() * 180;
+         
+
+//         if(e.target.id=buttn)
+//             buttn.style.background = `rgb(${x + 10},${z + 2},${y + 12})`;
+
+//             check.className= 'fas fa-check-circle';
+        
+//         }, 1000);
+        
+//         check.remove= 'fas fa-check-circle';
+
+
+
+
+     
+//     }
+  
+ 
